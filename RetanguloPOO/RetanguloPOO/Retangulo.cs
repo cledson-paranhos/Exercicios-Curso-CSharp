fc14c544c1a729b867c36cad5fb572fac9dce57c
Cledson Paranhos <cledsonvgp@hotmail.com>
2020-06-30 20:15:24 -0300

    commit do exercicio

---
+++ RetanguloPOO/RetanguloPOO/Retangulo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RetanguloPOO
+{
+    class Retangulo
+    {
+        public double Largura;
+        public double Altura;
+
+        public double Area()
+        {
+            return Largura * Altura;
+        }
+        public double Perimetro()
+        {
+            return (Largura + Largura) + ( Altura + Altura);
+        }
+        public double Diagonal()
+        {
+            double lado1 = Math.Pow(Largura, 2);
+            double lado2 = Math.Pow(Altura, 2);
+            double total = lado1 + lado2;
+            return Math.Sqrt(total);
+        }
+    }
+}
