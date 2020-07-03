b00306625dc98647ebc120da64ba07b7ce20d583
Cledson Paranhos <cledsonvgp@hotmail.com>
2020-07-03 18:50:50 -0300

    commit

---
+++ InteracaoComUsuario/InteracaoComUsuario/divisores/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace divisores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            for(int i = 1; i <= N; i++)
+            {
+                if(N % i == 0 )
+                {
+                    Console.WriteLine(i);
+                    
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
