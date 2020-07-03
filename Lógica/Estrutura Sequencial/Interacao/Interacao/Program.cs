c8a40f24e086d22eceb66d1036bc925f560e50e0
Cledson Paranhos <cledsonvgp@hotmail.com>
2020-07-03 17:46:27 -0300

    commit

---
+++ Lógica/Estrutura Sequencial/Interacao/Interacao/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Interacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
