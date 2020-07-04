3547da116823eabdbd5abd02b2176bf0dfc652eb
Cledson Paranhos <cledsonvgp@hotmail.com>
2020-07-04 14:52:09 -0300

    Commit exercicio realizado

---
+++ Programação orientado a objetos/RetanguloPOO/RetanguloPOO/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace RetanguloPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo R = new Retangulo();
+
+            Console.WriteLine("Entre a largura e altura do retângulo: ");
+            R.Largura = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            R.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("AREA = " + R.Area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PERÍMETRO = " + R.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("DIAGONAL = " + R.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+            Console.ReadKey();
+        }
+    }
+}
