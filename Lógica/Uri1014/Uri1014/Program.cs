014335361bbf243986744d95051b7fca946629ff
Cledson Paranhos <cledsonvgp@hotmail.com>
2020-07-04 17:47:34 -0300

    Commit exercicio realizado

---
+++ Lógica/Uri1014/Uri1014/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace Uri1014
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            double y = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double consumo = x / y;
+            Console.WriteLine(consumo.ToString("F3",CultureInfo.InvariantCulture) + " km/l");
+
+            Console.ReadKey();
+        }
+    }
+}
