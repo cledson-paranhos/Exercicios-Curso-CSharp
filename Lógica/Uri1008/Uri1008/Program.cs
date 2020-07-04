2aae60279e6bdb3ba6066487891898611d42654b
Cledson Paranhos <cledsonvgp@hotmail.com>
2020-07-04 15:46:57 -0300

    Commit exercicio realizado

---
+++ Lógica/Uri1008/Uri1008/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace Uri1008
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroFuncionario, horasTrabalhadas;
+            double valor;
+            numeroFuncionario = int.Parse(Console.ReadLine());
+            horasTrabalhadas = int.Parse(Console.ReadLine());
+            valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double salario = horasTrabalhadas * valor;
+
+            Console.WriteLine("NUMBER = "+numeroFuncionario);
+            Console.WriteLine("SALARY = U$ "+salario.ToString("F2",CultureInfo.InvariantCulture));
+
+            Console.ReadKey();
+            
+
+        }
+    }
+}
