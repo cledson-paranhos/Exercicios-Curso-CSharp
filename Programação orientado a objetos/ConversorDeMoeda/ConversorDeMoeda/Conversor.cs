0e7502cfc287b759e8ea161f68fc2be1c66fa52a
Cledson Paranhos <cledsonvgp@hotmail.com>
2020-07-04 15:02:30 -0300

    Commit exercicio realizado

---
+++ Programação orientado a objetos/ConversorDeMoeda/ConversorDeMoeda/Conversor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConversorDeMoeda
+{
+    class Conversor
+    {
+        public static double IOF = 6.0;
+
+        public static double ValorASerPago(double cotacao, double quantidade)
+        {
+            double total = cotacao * quantidade;
+            return total + total * IOF / 100.0;
+        }
+       
+    }
+}
---
+++ Programação orientado a objetos/ConversorDeMoeda/ConversorDeMoeda/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConversorDeMoeda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual a cotação do dolar? ");
+            double cotacao = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.Write("Quantos dolares você vai comprar? ");
+            double quantidade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double resultado = Conversor.ValorASerPago(cotacao, quantidade);
+
+            Console.WriteLine("Valor a ser pago em reais " + resultado.ToString("F2", CultureInfo.InvariantCulture));
+
+          
+            
+            Console.ReadKey();
+        }
+        
+    }
+}
