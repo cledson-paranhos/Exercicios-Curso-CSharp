252272d630d67662de70e33dbc05d9e3d6ee484f
Cledson Paranhos <cledsonvgp@hotmail.com>
2020-07-06 20:01:21 -0300

    Commit exercicio realizado

---
+++ Lógica/Uri1012/Uri1012/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace Uri1012
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi = 3.14159;
+            string[] valores = Console.ReadLine().Split(' ');
+            double a = double.Parse(valores[0],CultureInfo.InvariantCulture);
+            double b = double.Parse(valores[1],CultureInfo.InvariantCulture);
+            double c = double.Parse(valores[2],CultureInfo.InvariantCulture);
+
+            double trianguloRetangulo = a * c / 2.0;
+            double circulo = pi * c * c;
+            double trapezio = (a + b) / 2 * c;
+            double quadrado = b * b;
+            double retangulo = a * b;
+
+            Console.WriteLine("TRIANGULO: "+ trianguloRetangulo.ToString("F3",CultureInfo.InvariantCulture));
+            Console.WriteLine("CIRCULO: "+ circulo.ToString("F3",CultureInfo.InvariantCulture));
+            Console.WriteLine("TRAPEZIO: "+trapezio.ToString("F3",CultureInfo.InvariantCulture));
+            Console.WriteLine("QUADRADO: "+quadrado.ToString("F3",CultureInfo.InvariantCulture));
+            Console.WriteLine("RETANGULO: " + retangulo.ToString("F3",CultureInfo.InvariantCulture));
+
+            Console.ReadKey();
+        }
+    }
+}
