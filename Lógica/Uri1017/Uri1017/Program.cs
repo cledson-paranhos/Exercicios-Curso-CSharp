e8936d1d0f1ac035c1a1f27935d1524b85e5ea83
Cledson Paranhos <cledsonvgp@hotmail.com>
2020-07-06 13:33:20 -0300

    Commit exercicio realizado

---
+++ Lógica/Uri1017/Uri1017/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace Uri1017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int horas = int.Parse(Console.ReadLine());
+            int velocidade = int.Parse(Console.ReadLine());
+
+            double consumo = (horas * velocidade / 12.0);
+            Console.WriteLine(consumo.ToString("F3",CultureInfo.InvariantCulture));
+            Console.ReadKey();
+
+
+        }
+    }
+}
