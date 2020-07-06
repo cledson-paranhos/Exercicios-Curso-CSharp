46e3497936de88736f74421f2f9d7666f736b108
Cledson Paranhos <cledsonvgp@hotmail.com>
2020-07-06 14:25:25 -0300

    Commit exercicio realizado

---
+++ Lógica/Uri1006/Uri1006/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace Uri1006
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double A = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            double B = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            double C = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            double media = (A * 2.0 + B * 3.0 + C * 5.0) / 10.0;
+            Console.WriteLine("MEDIA = "+media.ToString("F1",CultureInfo.InvariantCulture));
+            Console.ReadKey();
+        }
+    }
+}
