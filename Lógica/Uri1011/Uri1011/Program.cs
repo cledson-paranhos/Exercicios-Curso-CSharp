626ca6e534ed84b3c15d229dd543846a2e124370
Cledson Paranhos <cledsonvgp@hotmail.com>
2020-07-06 18:55:44 -0300

    Commit exercicio realizado

---
+++ Lógica/Uri1011/Uri1011/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace Uri1011
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double pi = 3.14159;
+            int r = int.Parse(Console.ReadLine());
+            double calculo = 4.0 / 3.0 * pi * r * r * r;
+            Console.WriteLine("VOLUME = "+calculo.ToString("F3",CultureInfo.InvariantCulture));
+
+            Console.ReadKey();
+        }
+    }
+}
