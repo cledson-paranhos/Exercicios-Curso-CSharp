46b7cce708d2ebc65d525864cad87be44ee7cb42
Cledson Paranhos <cledsonvgp@hotmail.com>
2020-07-06 14:08:30 -0300

    Commit exercicio realizado

---
+++ Lógica/Uri1005/Uri1005/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace Uri1005
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double media = 0;
+            double A = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            double B = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            media = (A * 3.5 + B * 7.5) / 11;
+            
+            Console.WriteLine("MEDIA = " + media.ToString("F5",CultureInfo.InvariantCulture));
+            Console.ReadKey();
+        }
+    }
+}
