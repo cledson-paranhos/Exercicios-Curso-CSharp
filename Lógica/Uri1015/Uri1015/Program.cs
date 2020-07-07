9d8e2c9cf2737ac15d38f388e5a45f8b08944580
Cledson Paranhos <cledsonvgp@hotmail.com>
2020-07-07 10:39:24 -0300

    Commit exercicio realizado

---
+++ Lógica/Uri1015/Uri1015/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace Uri1015
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] valores1 = Console.ReadLine().Split(' ');
+            double x1 = double.Parse(valores1[0], CultureInfo.InvariantCulture);
+            double y1 = double.Parse(valores1[1], CultureInfo.InvariantCulture);
+
+            string[] valores2 = Console.ReadLine().Split(' ');
+            double x2 = double.Parse(valores2[0], CultureInfo.InvariantCulture);
+            double y2 = double.Parse(valores2[1], CultureInfo.InvariantCulture);
+
+            double distancia = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+
+            Console.WriteLine(distancia.ToString("F4",CultureInfo.InvariantCulture));
+            Console.ReadKey();
+        }
+    }
+}
