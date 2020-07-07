ce03ba227a774a8b6c60bdb542f266a24377124e
Cledson Paranhos <cledsonvgp@hotmail.com>
2020-07-07 10:26:02 -0300

    Commit exercicio realizado

---
+++ Lógica/Uri1013/Uri1013/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Uri1013
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] valores = Console.ReadLine().Split(' ');
+            int a = int.Parse(valores[0]);
+            int b = int.Parse(valores[1]);
+            int c = int.Parse(valores[2]);
+
+            int MaiorAB = (a + b + Math.Abs(a - b)) / 2;
+            MaiorAB = (MaiorAB + c + Math.Abs(MaiorAB - c)) / 2;
+            Console.WriteLine(MaiorAB + " eh o maior");
+
+            Console.ReadKey();
+        }
+    }
+}
