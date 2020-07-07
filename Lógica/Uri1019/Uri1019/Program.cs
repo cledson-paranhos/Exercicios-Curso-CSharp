e23a47d5ce4d81432fe27f480a621e7267ad8618
Cledson Paranhos <cledsonvgp@hotmail.com>
2020-07-07 18:11:25 -0300

    Commit exercicio realizado

---
+++ Lógica/Uri1019/Uri1019/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Uri1019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int horas = n / 3600;
+            int resto = n % 3600;
+
+            int minutos = resto / 60;
+            int segundos = resto % 60;
+
+            Console.WriteLine(horas + ":"+ minutos+":"+ segundos);
+
+            Console.ReadKey();
+
+        }
+    }
+}
