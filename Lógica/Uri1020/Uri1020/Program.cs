3a8df7d5f840f3e68416b6589cd2aa30a588bd4c
Cledson Paranhos <cledsonvgp@hotmail.com>
2020-07-07 18:58:50 -0300

    Commit exercicio realizado

---
+++ Lógica/Uri1020/Uri1020/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Uri1020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = int.Parse(Console.ReadLine());
+
+            int anos = i / 365;
+            int anosMod = i % 365;
+
+            int meses = anosMod / 30;
+            int dias = anosMod % 30;
+
+            Console.WriteLine(anos + " ano(s)");
+            Console.WriteLine(meses + " mes(es)");
+            Console.WriteLine(dias + " dia(s)");
+            
+            Console.ReadKey();
+        }
+    }
+}
