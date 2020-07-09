75095f0bd09d769ceeb22920240714341c315541
Cledson Paranhos <cledsonvgp@hotmail.com>
2020-07-09 17:02:01 -0300

    Commit exercicio realizado

---
+++ Lógica/Uri1044/Uri1044/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Uri1044
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vet = Console.ReadLine().Split(' ');
+            int a = int.Parse(vet[0]);
+            int b = int.Parse(vet[1]);
+
+            if(a % b == 0 || b % a == 0)
+            {
+                Console.WriteLine("Sao Multiplos");
+            }
+            else
+            {
+                Console.WriteLine("Nao sao Multiplos");
+            }
+            Console.ReadKey();
+        }
+    }
+}
