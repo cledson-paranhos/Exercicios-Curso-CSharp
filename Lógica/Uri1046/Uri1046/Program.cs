e938973b74c9b1aa8b56ceafb8954684ff1ab75c
Cledson Paranhos <cledsonvgp@hotmail.com>
2020-07-09 18:03:21 -0300

    Commit exercicio realizado

---
+++ Lógica/Uri1046/Uri1046/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Uri1046
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vet = Console.ReadLine().Split(' ');
+            int horaInicio = int.Parse(vet[0]);
+            int horaFinal = int.Parse(vet[1]);
+            int horaTotal = 24;
+            
+            if(horaInicio > horaFinal)
+            {
+                Console.WriteLine("O JOGO DUROU " + (horaTotal - horaInicio + horaFinal)+ " HORA(S)");
+            }else if(horaInicio == horaFinal)
+            {
+                Console.WriteLine("O JOGO DUROU " + horaTotal +" HORA(S)");
+            }
+            else
+            {
+                Console.WriteLine("O JOGO DUROU " + (horaFinal - horaInicio) + " HORA(S)");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
