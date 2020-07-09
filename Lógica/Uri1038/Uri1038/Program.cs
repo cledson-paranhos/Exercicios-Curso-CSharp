1fde43dbbe17e8748f76624ae48a4b9d562b5eea
Cledson Paranhos <cledsonvgp@hotmail.com>
2020-07-09 15:57:20 -0300

    Commit exercicio realizado

---
+++ Lógica/Uri1038/Uri1038/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+namespace Uri1038
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lanchonete = Console.ReadLine().Split(' ');
+            int cod = int.Parse(lanchonete[0]);
+            int quant = int.Parse(lanchonete[1]);
+            double valorTotal = 0.0;
+
+            if(cod == 1)
+            {
+                valorTotal = quant * 4.00;
+            }else if (cod == 2)
+            {
+                valorTotal = quant * 4.50;
+            }else if (cod == 3)
+            {
+                valorTotal = quant * 5.00;
+            }else if(cod == 4)
+            {
+                valorTotal = quant * 2.00;
+
+            }
+            else
+            {
+                valorTotal = quant * 1.50;
+
+            }
+            Console.WriteLine("Total: R$ "+ valorTotal.ToString("F2",CultureInfo.InvariantCulture));
+            Console.ReadKey();
+        }
+    }
+}
