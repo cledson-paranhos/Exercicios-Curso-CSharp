cd6ae8f791077292a4a892b89470bc81f7735532
Cledson Paranhos <cledsonvgp@hotmail.com>
2020-07-10 11:27:14 -0300

    Commit exercicio realizado

---
+++ Lógica/Uri1037/Uri1037/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace Uri1037
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valores = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            if(valores < 0 || valores > 100)
+            {
+                Console.WriteLine("Fora de intervalo");
+            }else if(valores <= 25.0)
+            {
+                Console.WriteLine("Intervalo [0,25]");
+            }else if (valores <= 50.0)
+            {
+                Console.WriteLine("Intervalo (25,50]");
+            }else if (valores <= 75.0)
+            {
+                Console.WriteLine("Intervalo (50,75]");
+            }
+            else
+            {
+                Console.WriteLine("Intervalo (75,100]");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
