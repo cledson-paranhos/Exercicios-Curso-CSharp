31346af7a26eebe6adfd4a9d7202a41d2831dc9b
Cledson Paranhos <cledsonvgp@hotmail.com>
2020-07-10 10:54:01 -0300

    Commit exercicio realizado

---
+++ Lógica/Uri1048/Uri1048/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Globalization;
+
+namespace Uri1048
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double novoSalario, reajusteGanho;
+
+            double salario = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            if(salario <= 400.0)
+            {
+                double perc = 15;
+                novoSalario = (salario * perc) / 100 + salario;
+                reajusteGanho = salario * perc / 100;
+
+                Console.WriteLine("Novo salario: "+ novoSalario.ToString("F2",CultureInfo.InvariantCulture));
+                Console.WriteLine("Reajuste ganho: "+ reajusteGanho.ToString("F2",CultureInfo.InvariantCulture));
+                Console.WriteLine("Em percentual: "+ perc + " %");
+            }
+            else if(salario <= 800.0)
+            {
+                double perc = 12;
+                novoSalario = (salario * perc) / 100 + salario;
+                reajusteGanho = salario * perc / 100;
+
+                Console.WriteLine("Novo salario: " + novoSalario.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("Reajuste ganho: " + reajusteGanho.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("Em percentual: " + perc + " %");
+            }else if (salario <= 1200.0)
+            {
+                double perc = 10;
+                novoSalario = (salario * perc) / 100 + salario;
+                reajusteGanho = salario * perc / 100;
+
+                Console.WriteLine("Novo salario: " + novoSalario.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("Reajuste ganho: " + reajusteGanho.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("Em percentual: " + perc + " %");
+            }else if(salario <= 2000.0)
+            {
+                double perc = 7;
+                novoSalario = (salario * perc) / 100 + salario;
+                reajusteGanho = salario * perc / 100;
+
+                Console.WriteLine("Novo salario: " + novoSalario.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("Reajuste ganho: " + reajusteGanho.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("Em percentual: " + perc + " %");
+            }
+            else
+            {
+                double perc = 4;
+                novoSalario = (salario * perc) / 100 + salario;
+                reajusteGanho = salario * perc / 100;
+
+                Console.WriteLine("Novo salario: " + novoSalario.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("Reajuste ganho: " + reajusteGanho.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("Em percentual: " + perc + " %");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
