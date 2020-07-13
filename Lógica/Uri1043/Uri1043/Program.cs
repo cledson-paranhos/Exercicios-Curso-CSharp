24836da163a9b4c4d31e2465072be915220be6b1
Cledson Paranhos <cledsonvgp@hotmail.com>
2020-07-13 16:26:34 -0300

    Commit exercicio realizado

---
+++ Lógica/Uri1043/Uri1043/Program.cs
@@ -0,0 +1,30 @@
+﻿ using System;
+using System.Globalization;
+
+namespace Uri1043
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double perimetro, area;
+            string[] vet = Console.ReadLine().Split(' ');
+            double a = double.Parse(vet[0],CultureInfo.InvariantCulture);
+            double b = double.Parse(vet[1],CultureInfo.InvariantCulture);
+            double c = double.Parse(vet[2],CultureInfo.InvariantCulture);
+
+            if(a < b + c && b < a + c && c < a + b)
+            {
+                perimetro = a + b + c;
+                Console.WriteLine("Perimetro = "+ perimetro.ToString("F1",CultureInfo.InvariantCulture));
+            }
+            else
+            {
+                area = ((a + b) * c) / 2;
+                Console.WriteLine("Area = "+ area.ToString("F1",CultureInfo.InvariantCulture));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
