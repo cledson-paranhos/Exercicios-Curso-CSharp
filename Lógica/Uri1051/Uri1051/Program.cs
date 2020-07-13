6412f704f7bbc5f19db15acfd610f53a67e62238
Cledson Paranhos <cledsonvgp@hotmail.com>
2020-07-13 14:25:15 -0300

    Commit exercicio realizado

---
+++ Lógica/Uri1051/Uri1051/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace Uri1051
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            double imposto = 0;
+
+            if(valor <= 2000.0)
+            {
+                Console.WriteLine("Isento");
+            }else if(valor <= 3000.0)
+            {
+                imposto = (valor - 2000.0) * 0.08;
+            }else if(valor <= 4500.0)
+            {
+                imposto = (valor - 3000.0) * 0.18 + 1000.0 * 0.08;
+            }
+            else
+            {
+                imposto = (valor - 4500.0) * 0.28 + 1500.0 * 0.18 + 1000.0 * 0.08;
+            }
+            if(valor > 2000.0)
+            {
+                Console.WriteLine("R$ " + imposto.ToString("F2", CultureInfo.InvariantCulture));
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
