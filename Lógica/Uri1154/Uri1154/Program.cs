8ec163f37471866e99655fcaaa1acfac3a0e2fe6
Cledson Paranhos <cledsonvgp@hotmail.com>
2020-07-15 12:33:02 -0300

    Commit exercicio realizado

---
+++ Lógica/Uri1154/Uri1154/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace Uri1154
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idades = int.Parse(Console.ReadLine());
+            int soma = 0;
+            double cont = 0.0;
+            while(idades > 0)
+            {
+                soma += idades;
+                cont++;
+                idades = int.Parse(Console.ReadLine());
+                
+            }
+            double media = soma / cont;
+            Console.WriteLine(media.ToString("F2",CultureInfo.InvariantCulture));
+
+            Console.ReadKey();
+        }
+    }
+}
