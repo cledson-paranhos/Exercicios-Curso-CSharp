5972fa8c79b59d95846077bea4062dad0235272f
Cledson Paranhos <cledsonvgp@hotmail.com>
2020-07-16 15:05:35 -0300

    Commit exercicio realizado

---
+++ Lógica/Uri1071/Uri1071/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Uri1071
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int min, max, soma = 0;
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+
+            if(x < y)
+            {
+                min = x;
+                max = y;
+            }
+            else
+            {
+                min = y;
+                max = x;
+            }
+            for(int i = min + 1; i < max; i++)
+            {
+                if(i % 2 != 0)
+                {
+                    soma = soma + i;
+                }
+            }
+            Console.WriteLine(soma);
+            Console.ReadKey();
+        }
+    }
+}
