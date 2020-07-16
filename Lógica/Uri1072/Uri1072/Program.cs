801b0ec981e9ddf12223104222dfb7beaaf76269
Cledson Paranhos <cledsonvgp@hotmail.com>
2020-07-16 20:33:55 -0300

    Commit exercicio realizado

---
+++ Lógica/Uri1072/Uri1072/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Uri1072
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dentro = 0, fora = 0;
+            int n = int.Parse(Console.ReadLine());
+            for(int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                if(x >= 10 && x <= 20)
+                {
+                    dentro++;
+                }
+                else
+                {
+                    fora++;
+                }
+            }
+            Console.WriteLine(dentro + " in");
+            Console.WriteLine(fora + " out");
+            Console.ReadKey();
+
+        }
+    }
+}
