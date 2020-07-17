2ad54ebe856df8310e4470b28614c9144bf3158b
Cledson Paranhos <cledsonvgp@hotmail.com>
2020-07-17 11:02:16 -0300

    Commit exercicio realizado

---
+++ Lógica/Uri1073/Uri1073/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Uri1073
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int quadrado = 0;
+            int n = int.Parse(Console.ReadLine());
+            for(int i = 1; i <= n; i++)
+            {
+                if(i % 2 == 0)
+                {
+                    quadrado = i * i;
+                    Console.WriteLine(i +"^"+ "2" +" = "+ quadrado );
+                }
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
