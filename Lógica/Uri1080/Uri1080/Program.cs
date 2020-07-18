474ad4a4907e0f0aae9b52806befb74efabd2796
Cledson Paranhos <cledsonvgp@hotmail.com>
2020-07-18 16:22:31 -0300

    Commit exercicio realizado

---
+++ Lógica/Uri1080/Uri1080/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Uri1080
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maior = int.Parse(Console.ReadLine());
+            int cont = 0;
+
+            for (int i = 2; i <= 100; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                if (x > maior)
+                {
+                    maior = x;
+                    cont = i;
+                }
+
+            }
+            Console.WriteLine(maior);
+            Console.WriteLine(cont);
+
+            Console.ReadKey();
+        }
+    }
+}
