50db83cf25781647c549fd3c2bef9c65de8472b6
Cledson Paranhos <cledsonvgp@hotmail.com>
2020-07-20 20:04:58 -0300

    Commit exercicio realizado

---
+++ Lógica/Uri1095/Uri1095/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Uri1095
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int j = 60;
+            for(int i = 1 ; i <= 37 ; i += 3)
+            {
+                Console.WriteLine("I="+i+ " J="+ (j));
+                j -= 5;  
+            }
+            Console.ReadKey();
+        }
+    }
+}
