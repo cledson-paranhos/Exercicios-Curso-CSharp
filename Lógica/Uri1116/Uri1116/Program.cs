e4462f206916bf6831485da89d710c93a41e5e1b
Cledson Paranhos <cledsonvgp@hotmail.com>
2020-07-20 19:38:40 -0300

    Commit exercicio realizado

---
+++ Lógica/Uri1116/Uri1116/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace Uri1116
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] vet = Console.ReadLine().Split(' ');
+                int X = int.Parse(vet[0]);
+                int Y = int.Parse(vet[1]);
+
+                double resultado = (double) X / Y;
+                if(resultado == 0.0)
+                {
+                    Console.WriteLine("0.0");
+                }else if(Y == 0)
+                {
+                    Console.WriteLine("divisao impossivel");
+                }
+                else 
+                {
+                    Console.WriteLine(resultado.ToString("F1", CultureInfo.InvariantCulture));
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
