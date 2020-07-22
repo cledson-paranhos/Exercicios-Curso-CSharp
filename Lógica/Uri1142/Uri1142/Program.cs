fcf02155a124effa1bc3fe17d9e320416573c32d
Cledson Paranhos <cledsonvgp@hotmail.com>
2020-07-22 13:24:28 -0300

    Commit exercicio realizado

---
+++ Lógica/Uri1142/Uri1142/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Uri1142
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 1;
+            int n = int.Parse(Console.ReadLine());
+            for(int i = 1; i <= n; i++)
+            {
+                
+                Console.WriteLine(numero + " "+ (numero += 1) +" " + (numero += 1) + " PUM");
+                numero += 2;
+            }
+            Console.ReadKey();
+        }
+    }
+}
