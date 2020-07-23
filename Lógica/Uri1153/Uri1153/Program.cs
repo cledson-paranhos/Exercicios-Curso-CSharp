c869df508d9fad9ffb08736f431b1a489fe41465
cledson-paranhos <cledsonvgp@hotmail.com>
2020-07-23 16:14:23 -0300

    Commit exercicio realizado

---
+++ Lógica/Uri1153/Uri1153/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Uri1153
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor = 1;
+            int n = int.Parse(Console.ReadLine());
+            for(int i = 1; i <= n; i++)
+            {
+                valor = valor * i;
+            }
+            Console.WriteLine(valor);
+            Console.ReadKey();
+        }
+    }
+}
