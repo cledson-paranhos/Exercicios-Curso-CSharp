e989ace017d608a89eeac76857c9f10d1f0d20ad
cledson-paranhos <cledsonvgp@hotmail.com>
2020-07-23 15:22:56 -0300

    Commit exercicio realizado

---
+++ Lógica/Uri1151/Uri1151/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Uri1151
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0, num2 = 1;
+            int n = int.Parse(Console.ReadLine());
+            for(int i = 0; i < n; i++)
+            {
+                if(i == n - 1)
+                {
+                    Console.WriteLine(num1);
+                }
+                else
+                {
+                    Console.Write(num1 + " ");
+                }
+                int aux = num2;
+                num2 = num1;
+                num1 = aux + num2;
+                
+            }
+            Console.ReadKey();
+        }
+    }
+}
