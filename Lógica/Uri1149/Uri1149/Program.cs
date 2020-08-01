53958fda63ae44875ae82c6e172d6aecb0bcf37e
Cledson Paranhos <cledsonvgp@hotmail.com>
2020-08-01 19:16:45 -0300

    Exercicio parcialmente realizado

---
+++ Lógica/Uri1149/Uri1149/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Uri1149
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int soma = 0, a, n, i =0;
+            string[] vet = Console.ReadLine().Split(' ');
+            a = int.Parse(vet[0]);
+            i = i+ 1;
+
+            n = int.Parse(vet[i]);
+            
+            while(n <= 0)
+            {
+                //i = i + 1;
+                n = int.Parse(vet[i]);
+            }
+            
+            for( i = 0; i < n; i++)
+            {
+
+                soma = soma + a + i;
+                
+            }
+            
+            Console.WriteLine(soma);
+        }
+    }
+}
