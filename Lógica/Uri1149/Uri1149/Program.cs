b42a0047420c79244cbc3f114ec345d0151e9f0c
Cledson Paranhos <cledsonvgp@hotmail.com>
2020-08-02 11:33:40 -0300

    Commit exercicio realizado

--- Lógica/Uri1149/Uri1149/Program.cs
+++ Lógica/Uri1149/Uri1149/Program.cs
@@ -6,27 +6,24 @@ namespace Uri1149
     {
         static void Main(string[] args)
         {
-            int soma = 0, a, n, i =0;
-            string[] vet = Console.ReadLine().Split(' ');
-            a = int.Parse(vet[0]);
-            i = i+ 1;
+            int soma = 0, a, n, i = 0;
+            string[] valores = Console.ReadLine().Split(' ');
+            a = int.Parse(valores[0]);
+            i = 1;
 
-            n = int.Parse(vet[i]);
-            
+            n = int.Parse(valores[i]);
+
             while(n <= 0)
             {
-                //i = i + 1;
-                n = int.Parse(vet[i]);
+                i = i + 1;
+                n = int.Parse(valores[i]);
             }
-            
-            for( i = 0; i < n; i++)
+            for(i = 0; i < n; i++)
             {
-
                 soma = soma + a + i;
-                
             }
-            
             Console.WriteLine(soma);
+
         }
     }
 }
