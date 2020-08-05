4bedb647947779757aad58e019310ace0d733eb4
Cledson Paranhos <cledsonvgp@hotmail.com>
2020-08-05 13:06:38 -0300

    Commit exercicio realizado

---
+++ Lógica/Uri1158/Uri1158/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace Uri1158
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int soma = 0, maior= 0, menor = 0;
+            int n = int.Parse(Console.ReadLine()); 
+            for (int i = 0; i < n; i++)
+            {
+                string[] valores = Console.ReadLine().Split();
+                int x = int.Parse(valores[0]);
+                int y = int.Parse(valores[1]);
+
+                if(x % 2 == 0)
+                {
+                    x = x + 1;
+                }
+                for(int j = 0; j < y; j++)
+                {
+                    soma = soma + x;
+                    x += 2;
+                }
+                Console.WriteLine(soma);
+                soma = 0; 
+            }
+        }
+    }
+}
