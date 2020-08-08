b3eb58aaf2bbf66e47ccf8861b4a35e883caa0f5
Cledson Paranhos <cledsonvgp@hotmail.com>
2020-08-08 13:19:58 -0300

    Commit exercicio realizado

---
+++ Lógica/Vetor3/Vetor3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Vetor3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] a;
+            int[] b;
+            int[] c;
+            int n = int.Parse(Console.ReadLine());
+            a = new int[n];
+            b = new int[n];
+            c = new int[n];
+            string[] s = Console.ReadLine().Split(' ');
+            for(int i = 0; i< n; i++)
+            {
+                a[i] = int.Parse(s[i]);
+            }
+
+            s = Console.ReadLine().Split(' ');
+
+            for(int i = 0; i < n; i++)
+            {
+                b[i] = int.Parse(s[i]);
+            }
+
+            for(int i = 0; i< n; i++)
+            {
+                c[i] = a[i] + b[i];
+                Console.Write(c[i] + " ");
+            }
+            
+
+        }
+    }
+}
