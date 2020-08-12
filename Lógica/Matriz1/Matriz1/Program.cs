c68a1312886b75089504d451980fec6224b81e0a
Cledson Paranhos <cledsonvgp@hotmail.com>
2020-08-12 16:15:52 -0300

    Commit exercicio realizado

---
+++ Lógica/Matriz1/Matriz1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Matriz1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split(' ');
+            int m = int.Parse(s[0]);
+            int n = int.Parse(s[1]);
+            int[,] a = new int[m, n];
+
+            for(int i = 0; i<m; i++)
+            {
+                s = Console.ReadLine().Split(' ');
+                for(int j = 0; j<n; j++)
+                {
+                    a[i, j] = int.Parse(s[j]);
+                }
+            }
+            Console.WriteLine("VALORES NEGATIVOS: ");
+
+            for(int i = 0; i < m; i++)
+            {
+                for(int j =0; j<n; j++)
+                {
+                    if(a[i,j] < 0)
+                    {
+                        Console.WriteLine(a[i,j]);
+                    }
+                }
+            }
+        }
+    }
+}
