5b55399200a560bcaa2a923710036d98650f7e9b
Cledson Paranhos <cledsonvgp@hotmail.com>
2020-08-12 18:37:38 -0300

    Commit exercicio realizado

---
+++ Lógica/Matriz2/Matriz2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace Matriz2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] mat = new int[n, n];
+
+            for(int i = 0; i < n; i++)
+            {
+                string[] s = Console.ReadLine().Split(' ');
+                for(int j = 0; j < n; j++)
+                {
+                    mat[i, j] = int.Parse(s[j]);
+                }
+            }
+            for(int i = 0; i < n; i++)
+            {
+                int soma = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    soma = soma + mat[i,j];
+                }
+                Console.WriteLine(soma);
+            }     
+        }
+    }
+}
