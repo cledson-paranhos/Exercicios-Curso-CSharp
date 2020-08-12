035db0b273ef221df4d74d3c24d9334ece9d9dc9
Cledson Paranhos <cledsonvgp@hotmail.com>
2020-08-12 19:18:03 -0300

    Commit exercicio realizado

---
+++ Lógica/Matriz3/Matriz3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Matriz3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] mat = new int[n, n];
+            
+            for(int i = 0; i<n; i++)
+            {
+                string[] s = Console.ReadLine().Split(' ');
+                for(int j = 0; j < n; j++)
+                {
+                    mat[i, j] = int.Parse(s[j]);
+                }
+            }
+            int numMaior = 0;
+            for (int i = 0; i<n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    
+                    if(mat[i,j] > numMaior)
+                    {
+                        numMaior = mat[i, j];
+                    }   
+                }
+                Console.WriteLine(numMaior);
+                numMaior = 0;
+            }       
+        }
+    }
+}
