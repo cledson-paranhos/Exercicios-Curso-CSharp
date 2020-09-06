1a1624dfe45c8e5a6e08ff22f67c5d4bc6a6d985
cledson-paranhos <cledsonvgp@hotmail.com>
2020-09-06 18:16:09 -0300

    Commit exercicio realizado

---
+++ Programação orientado a objetos/Retangulo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace Retangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo R = new Retangulo();
+            Console.WriteLine("Entre com a largura e altura do retângulo:");
+            R.Largura = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            R.Altura = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            Console.Write("AREA: "+ R.Area().ToString("F2",CultureInfo.InvariantCulture));
+            Console.WriteLine();
+
+            Console.Write("PERIMETRO: " + R.Perimetro().ToString("F2",CultureInfo.InvariantCulture));
+            Console.WriteLine();
+
+            Console.Write("DIAGONAL: "+R.Diagonal().ToString("F2",CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ Programação orientado a objetos/Retangulo/Retangulo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Retangulo
+{
+    class Retangulo
+    {
+        public double Largura;
+        public double Altura;
+
+        public double Area()
+        {
+            return Largura * Altura;
+        }
+
+        public double Perimetro()
+        {
+            return Largura + Largura + Altura + Altura;
+        }
+        public double Diagonal()
+        {
+            return Math.Sqrt(Largura * Largura + Altura * Altura);
+        }
+    }
+}
