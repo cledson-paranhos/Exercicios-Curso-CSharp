139630b9cf0f6b219a146ba7397492085b00c271
cledson-paranhos <cledsonvgp@hotmail.com>
2020-09-07 11:12:47 -0300

    Commit exercicio realizado

---
+++ Programação orientado a objetos/Funcionario/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace Funcionario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario F = new Funcionario();
+            Console.Write("Nome: ");
+            F.Nome = Console.ReadLine();
+
+            Console.Write("Salário bruto: ");
+            F.SalarioBruto = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            
+            Console.Write("Imposto: ");
+            F.Imposto = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            Console.Write("Funcionário: "+F);
+            Console.WriteLine();
+
+            Console.Write("Deseja aumentar o salário em qual porcentagem? ");
+            double porce = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            F.AumentarSalario(porce);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Dados atualizados: "+ F);
+        }
+    }
+}
