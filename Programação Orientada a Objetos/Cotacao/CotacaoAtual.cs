7e881a73d66fa15656a41df65edba592845ff577
cledson-paranhos <cledsonvgp@hotmail.com>
2020-09-13 10:00:59 -0300

    Commit exercicio realizado

---
+++ Programação Orientada a Objetos/Cotacao/CotacaoAtual.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Cotacao
+{
+    class CotacaoAtual
+    {
+        public static double IOF = 6.0;
+
+        public static double ValorTotal(double valor, double quant)
+        {
+            double calculo = valor * quant * IOF /100;
+            return calculo + valor * quant;
+        }
+    }
+}
---
+++ Programação Orientada a Objetos/Cotacao/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Cotacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é a cotação do dolar? ");
+            double valorDolar = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.WriteLine();
+
+            Console.Write("Quantos dólares você vai comprar? ");
+            double quantidade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine();
+
+            Console.WriteLine("Valor a ser pago em reais = "+ CotacaoAtual.ValorTotal(valorDolar, quantidade).ToString("F2",CultureInfo.InvariantCulture)); 
+        }
+    }
+}
