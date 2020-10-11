84e72bd42e4625b580844b32d0acede41648ff96
Cledson Paranhos <cledsonvgp@hotmail.com>
2020-10-11 10:25:37 -0300

    Commit exercicio realizado

---
+++ Programação Orientada a Objetos/Encapsulamento/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Encapsulamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto("TV", 500.00, 10);
+            p.GetNome();
+            Console.WriteLine(p);
+            Console.WriteLine(p.GetPreco());
+        }
+    }
+}
