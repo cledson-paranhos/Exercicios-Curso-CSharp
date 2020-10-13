81870922eb684cf0d36c20de9d97eb2c3f67b448
Cledson Paranhos <cledsonvgp@hotmail.com>
2020-10-13 20:39:52 -0300

    Commit exercicio realizado

---
+++ Programação Orientada a Objetos/Properties/Produto.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+namespace Properties
+{
+    class Produto
+    {
+        private string _nome;
+        private double _preco;
+        private int _quantidade;
+
+        public Produto()
+        {
+
+        }
+        public Produto(string nome, double preco, int quantidadade)
+        {
+            _nome = nome;
+            _preco = preco;
+            _quantidade = quantidadade;
+        }
+        public string Nome
+        {
+            get{ return _nome; }
+            set
+            {
+                if(value != null && value.Length > 1)
+                {
+                    _nome = value;
+                }
+            }
+        }
+        public double Preco
+        {
+            get { return _preco; }
+        }
+
+        public double Quantidade
+        {
+            get { return _quantidade; }
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            return _preco * _quantidade;
+        }
+        public override string ToString()
+        {
+            return _nome + ", $ " + _preco.ToString("F2", CultureInfo.InvariantCulture) + ", " + _quantidade + " unidades, Total: " + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+        public void AdicionarProdutos(int quantidade)
+        {
+            _quantidade += quantidade;
+        }
+        public void RemoverProdutos(int quantidade)
+        {
+            _quantidade -= quantidade;
+        }
+    }
+}
---
+++ Programação Orientada a Objetos/Properties/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto P = new Produto("TV", 500.00, 10);
+
+            P.Nome = "T";
+
+            Console.Write("Nome: "+P.Nome);
+            Console.WriteLine();
+            
+            Console.Write("Preco: "+ P.Preco);
+            Console.WriteLine();
+
+            Console.WriteLine("Quantidade: "+ P.Quantidade);
+
+        }
+    }
+}
