667e09e27d7f32f056bdefae829491dabb9f9b72
Cledson Paranhos <cledsonvgp@hotmail.com>
2020-10-21 20:40:54 -0300

    Commit exercicio realizado

---
+++ Lógica/Uri1041/Uri1041/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+
+namespace Uri1041
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split(' ');
+            double x = double.Parse(s[0], CultureInfo.InvariantCulture);
+            double y = double.Parse(s[1], CultureInfo.InvariantCulture);
+
+            if (x == 0.0 && y == 0)
+            {
+                Console.WriteLine("Origem");
+            }
+            else if(x == 0)
+            {
+                Console.WriteLine("Eixo Y");
+            }else if (y == 0)
+            {
+                Console.WriteLine("Eixo X");
+            }
+            else if (x > 0 && y > 0)
+            {
+                Console.WriteLine("Q1");
+            }
+            else if (x > 0 && y < 0)
+            {
+                Console.WriteLine("Q4");
+            }
+            else if (x < 0 && y > 0)
+            {
+                Console.WriteLine("Q2");
+            }
+            else
+            {
+                Console.WriteLine("Q3");
+            }
+        }
+    }
+}
