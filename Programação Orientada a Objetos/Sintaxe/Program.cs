08dcbf1fe5c263dbef171992433c732f970a0cc9
Cledson Paranhos <cledsonvgp@hotmail.com>
2020-11-09 20:48:38 -0300

    Exercicio realizado

---
+++ Programação Orientada a Objetos/Sintaxe/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Sintaxe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto(){
+                Nome = "TV 4k",
+                Preco = 500.0,
+                Quantidade = 5
+            };
+            Console.Write("Dados atualizados: "+p);
+            Console.WriteLine();
+            
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int quant = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(quant);
+
+            Console.WriteLine("Dados atualizados: "+p);
+            Console.WriteLine();
+
+            Console.Write("Digite a quantidade a ser removido do estoque: ");
+            quant = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(quant);
+
+            Console.WriteLine("Dados atualizado: "+p);
+
+        }
+    }
+}
