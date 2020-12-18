77734660351bdea629f0a3834e252a73a7bee4d3
Cledson Paranhos <cledsonvgp@hotmail.com>
2020-12-17 21:38:19 -0300

    Commit exercicio realizado

---
+++ Programação Orientada a Objetos/Tipo-Referencia/Prod.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tipo_Referencia
+{
+    class Prod
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public Prod(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", " + Preco + ", " + Quantidade;
+        }
+    }
+}
---
+++ Programação Orientada a Objetos/Tipo-Referencia/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Tipo_Referencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tipo referencia não é usado como caixa, mas sim como ponteiro.
+
+            Prod P = new Prod("TV", 500.0, 10);
+
+            Prod p1 = P;
+
+            Console.WriteLine(P);
+            
+            Console.WriteLine(p1);
+
+            // Nesse caso o P1 está apontando para a variavel P que já foi instanciada. 
+        }
+    }
+}
