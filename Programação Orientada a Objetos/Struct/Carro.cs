b741530fcda2dd6ff8362da0a313688b04b49f8c
Cledson Paranhos <cledsonvgp@hotmail.com>
2020-12-20 15:46:53 -0300

    Commit exercicio realizado

---
+++ Programação Orientada a Objetos/Struct/Carro.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+namespace Struct
+{
+    struct Carro
+    {
+        //atributos
+        public string Marca;
+        public string Modelo;
+        public string Cor;
+        public double Valor;
+
+        //construtor com 4 parametros
+
+        public Carro(string marca, string modelo, string cor, double valor)
+        {
+            Marca = marca;
+            Modelo = modelo;
+            Cor = cor;
+            Valor = valor;
+        }
+
+        public void Info()
+        {
+            Console.WriteLine("Marca: "+Marca);
+            Console.WriteLine("Modelo: "+Modelo);
+            Console.WriteLine("Cor: "+Cor);
+            Console.WriteLine("Valor: "+ Valor.ToString("F3",CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ Programação Orientada a Objetos/Struct/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Colocando os valores diretos, usando o struct sempre deve iniciar com os valores
+            Carro C;
+            C.Marca = "Toyota";
+            C.Modelo = "Hilux";
+            C.Cor = "Branca";
+            C.Valor = 80.000;
+
+            Console.WriteLine("Marca: "+C.Marca);
+            Console.WriteLine("Modelo: "+C.Modelo);
+            Console.WriteLine("Cor: "+C.Cor);
+            Console.WriteLine("Valor: "+ C.Valor.ToString("F3",CultureInfo.InvariantCulture));
+
+            //O struct pode ser usado de 2 maneiras, colcocando valores direto ou usando comando new
+            Console.WriteLine();
+            C = new Carro("Vw", "Golf", "Preto", 50.00);
+            C.Info();
+
+            
+            Console.WriteLine();
+            Carro C1 = new Carro("BMW", "X7", "Prata", 60.000);
+            C1.Info();
+            
+        }
+    }
+}
